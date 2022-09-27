002fc1ceec90871cc0b68f2d6d2ac29c162ca13c
Devileya <fadlyarif77@gmail.com>
2022-09-27 13:38:18 +0700

    methods and directives

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data() {
         return {
             firstName: "Arif",
             lastName: "Siregar"
         }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName}`
+        }
     }
 }).mount('#app')
+
+setTimeout(() => {
+    vm.lastName = "Fadly"
+}, 2000)
